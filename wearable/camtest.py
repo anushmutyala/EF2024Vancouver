f1e8833e4a988b881e8f65f267c30e8fcb2a4124
ellie <ytaghavi@yahoo.com>
2025-01-14 02:20:55 -0800

    streasm from camera without saving as a file

--- wearable/camtest.py
+++ wearable/camtest.py
@@ -1,11 +1,25 @@
 from picamzero import Camera
 import base64
 from time import sleep
+from io import BytesIO
+from PIL import Image
+
 
 cam = Camera()
 cam.start_preview()
-cam.pc2.capture_file("image.jpg")
-with open("image.jpg", "rb") as image_file:
-        encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
-print(encoded_image)
+
+frame = cam.capture_array()  # Capture an image as a numpy array
+
+buffer = BytesIO()
+image = Image.fromarray(frame)  # Convert the numpy array to a PIL Image
+image.save(buffer, format="JPEG")
+buffer.seek(0)
+
+# Convert the image to base64
+image_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+
+
 sleep(5)
+f = open("test.txt", 'w')
+f.write(image_base64)
+f.close()
